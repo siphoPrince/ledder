8346b39d6fa43c58f4dd7a450637b7d35dac1491
siphoprince <mabirimisesipho@gmail.com>
2025-07-31 00:15:18 +0200

    hero section structure front end

---
+++ ledder/app/hero.jsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+    return (
+      <>
+        <h1 className="text-3xl font-bold">Ledder</h1>
+        <p className="text-gray-500 text-xl">Track your job applications and never miss an opportunity</p>
+        <div className="text-right mt-4">
+        <button className="flex items-center gap-2 bg-black rounded text-white px-4 py-2">
+    + Add Application
+  </button>
+</div>
+
+      </>
+    );
+  };
+  
+  export default Hero;
+  
