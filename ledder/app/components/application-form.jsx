ca19ab7accb463e8f7e594f3a4365fc4cc3d95de
siphoprince <mabirimisesipho@gmail.com>
2025-08-03 14:29:22 +0200

    adding form and button

--- ledder/app/components/application-form.jsx
+++ ledder/app/components/application-form.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function ApplicationForm(){
     const navigate = useNavigate();
@@ -21,26 +21,87 @@ export default function ApplicationForm(){
     // when form is submitted
     const handleSubmit = (e)=>{
         e.preventDefault();
+        navigate("/")
     };
 
     return(
         <>
-            <div className="flex flex-col gap-5">
-            <button
-                onClick={() => navigate("/")}
-                className="text-black w-32 shadow-2xl rounded-2xl">
-                Return
-            </button>
+            <div className="flex flex-col gap-6 p-6">
+            {/* Return button */}
+                <button
+                    onClick={() => navigate("/")}
+                    className="text-white bg-black cursor-pointer w-32 py-2 px-4 shadow-lg rounded-xl"
+                >
+                    Return
+                </button>
+
+                {/* Heading */}
+                <div className="space-y-1">
+                    <h2 className="text-xl font-bold">Add New Application</h2>
+                    <small className="text-gray-500">
+                    Enter the details of your job application
+                    </small>
+                </div>
+
+                {/* Application details card */}
+                <div className="shadow-lg rounded-lg p-5 bg-white">
+                    <h5 className="font-semibold text-lg">Application Details</h5>
+                    <small className="text-gray-500">
+                    Fill in all the relevant information about your job application
+                    </small>
+
+                {/* Form detail */}
+                <div className="">
+                    <h3>Company Information</h3>
+                    <form className="flex flex-col gap-1" onSubmit={handleSubmit}>
+                    <label>
+                        Company Name*
+                        <input
+                            placeholder="Enter Company Name..."
+                            type="text"
+                            value={formData.companyName}
+                            onChange={(e) =>handleInputChange("companyName", e.target.value)}
+                            />
+                    </label>
+
+                    <label>
+                        contactPerson*
+                        <input
+                            placeholder="Enter Contact Person"
+                            type="text"
+                            value={formData.contactPerson}
+                            onChange={(e)=> handleInputChange("contactPerson", e.target.value)}
+                            />
+                    </label>
+
+                    <label>
+                        jobTitle*
+                        <input
+                            placeholder="Enter Job Title"
+                            type="text"
+                            value={formData.jobTitle}
+                            onChange={(e)=> handleInputChange("jobTitle", e.target.value)}
+                        />
+                    </label>
+
+                    <label>
+                        notes*
+                        <textarea
+                            placeholder="Enter any notes"
+                            type="text"
+                            value={formData.notes}
+                            onChange={(e)=> handleInputChange("notes", e.target.value)}
+                        />
+                    </label>
+
+                    <button type="submit">Submit</button>
+                    </form>
+                </div>
+                </div>
+
+                
             </div>
-            
-            <h2 className="font-bold">Add New Application</h2>
-            <small>Enter the details of your job application</small>
-            
-            <div  className="shadow-lg">
-                <h5 className="font-semibold">Application Details</h5>
-                <small>Fill in all the relevant information about your job application</small>
-            </div>
-        </>
+  </>
 
     );
 
--- ledder/app/components/cards.jsx
+++ ledder/app/components/cards.jsx
@@ -1,20 +1,20 @@
 const Cards = ()=> {
     return(
         <>
-            <div className="flex gap-2" p-15>
-                <div className="w-1/4 h-32 bg-blue-50 rounded-2xl">
+            <div className="flex gap-2">
+                <div className="w-1/4 h-32 bg-blue-200 rounded-2xl">
                     <h6>Total applications</h6>
                     <span>2</span>
                 </div>
-                <div className="w-1/4 h-32 bg-red-50 rounded-2xl">
+                <div className="w-1/4 h-32 bg-red-200 rounded-2xl">
                     <h6>Pending</h6>
                     <span className="text-red-500">0</span>
                 </div>
-                <div className="w-1/4 h-32 bg-pink-50 rounded-2xl">
+                <div className="w-1/4 h-32 bg-pink-200 rounded-2xl">
                     <h6>Interviews</h6>
                     <span className="text-pink-500">5</span>
                 </div>
-                <div className="w-1/4 h-32 bg-yellow-50 rounded-2xl">
+                <div className="w-1/4 h-32 bg-yellow-200 rounded-2xl">
                     <h6>Accepted</h6>
                     <span className="text-yellow-500">1</span>
                 </div>
