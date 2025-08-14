e0c24678d73e9de13e43e9df2d8c927f04360378
siphoprince <mabirimisesipho@gmail.com>
2025-08-14 16:40:06 +0200

    logo demo

--- ledder/app/hero.jsx
+++ ledder/app/hero.jsx
@@ -4,7 +4,13 @@ const Hero = () => {
   return (
     <>
       <div className="flex items-center justify-between p-15">
-        <h1 className="text-3xl font-bold">Ledder</h1>
+      
+        <h1 className="text-3xl font-bold flex flex-row">
+          <img 
+              src="/wooden-ladder-icon-vector.jpg" 
+              alt="Ladder Icon" 
+              className="w-10 h-10" 
+          />Ledder</h1>
         <Link
           to="/application-form"
           className="flex flex-row items-center gap-2 bg-black rounded text-white px-4 py-2 cursor-pointer"
