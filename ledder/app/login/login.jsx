46030ee44910e6f9b9f773d12a372e55e864aa33
siphoprince <mabirimisesipho@gmail.com>
2025-08-22 12:34:29 +0200

    backend integration

--- ledder/app/login/login.jsx
+++ ledder/app/login/login.jsx
@@ -2,10 +2,10 @@ const Login = () => {
   return (
     <>
       <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4">
-        {/* Welcome Section - Now on top */}
-        <div className="mb-8 w-full max-w-lg rounded-2xl bg-green-300 p-8 text-center text-white shadow-lg md:max-w-xl">
+        {/* Welcome Section */}
+        <div className="mb-8 w-full max-w-lg rounded-2xl bg-green-300 p-8 text-center text-black shadow-lg md:max-w-xl">
           <h2 className="text-2xl font-bold md:text-3xl">Welcome back Sipho!</h2>
-          <span className="mt-2 block text-sm md:text-base">
+          <span className="mt-2.5 block text-sm md:text-base">
             Don't have an account?{" "}
             <a href="#" className="font-bold underline transition duration-300 hover:text-green-700">
               Sign Up
@@ -30,7 +30,7 @@ const Login = () => {
           </div>
           <button
             type="submit"
-            className="mt-6 w-full rounded-2xl bg-green-600 p-2 text-white transition duration-300 hover:bg-green-700"
+            className="mt-6 w-full rounded-2xl bg-green-600 p-2 cursor-pointer text-white transition duration-300 hover:bg-green-700"
           >
             Sign In
           </button>
