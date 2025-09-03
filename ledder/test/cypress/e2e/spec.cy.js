0ca8284795e0c39b914621e4df02fa5f868d1242
sipho mabirimise <122831078+siphoPrince@users.noreply.github.com>
2025-09-03 12:33:49 +0200

    Update spec.cy.js

--- ledder/test/cypress/e2e/spec.cy.js
+++ ledder/test/cypress/e2e/spec.cy.js
@@ -1,39 +1,66 @@
 /// <reference types="cypress" />
 
-// tests the header section
-describe("header section", ()=>{
-    it('header section', () =>{
+// ============================
+// Header Section Tests
+// ============================
+
+describe("Header section", ()=>{
+    beforeEach(()=>{
         cy.visit('/')
+    })
+
+    it('Should display the correct title', () =>{
         cy.title().should('include', 'Ledder')
     })
 })
 
+// ===========================================
 // tests the responsiveness of the small screen size
-describe("is responsive on mobile", ()=>{
-    it("test if add button is visible on mobile", ()=>{
-        cy.viewport('iphone-8')
+// ===========================================
+describe("Responsive Design", ()=>{
+    beforeEach(()=>{
         cy.visit('/application-form')
+        cy.viewport('iphone-8')
+    })
+
+    it("test if add button is visible on mobile", ()=>{
         cy.contains('Contact Person *').should('be.visible')
     })
 })
 
-// test to see if the logo is visible on application form
-describe("hide logo on form", ()=>{
-    it('should not show logo', ()=>{
+// ============================================
+// Application form Tests
+// ============================================
+describe("Application form", ()=>{
+    beforeEach(()=>{
+        cy.visit('application-form')
+    })
+
+    it.skip('should not show logo', ()=>{
         cy.viewport('iphone-8')
-        cy.visit('/application-form')
         cy.contains('Ledder').should("not.be.visible")
     })
-})
 
-// testing out the application inputs positive values
-describe('application inputs correct info', ()=>{
-    it.only("should enter input for company  information section", ()=>{
-        cy.visit('/application-form')
+    it("should enter valid input", ()=>{
         cy.contains('Company Name *').find('input').type('Acer inc')
         cy.contains('Contact Person *').find('input').type('Paul')
         cy.contains('About Company *').find('textarea').type('This is a hardware and software company')
+        cy.contains('job Title *').find('input').type('Software Dev')
+        cy.contains('Contact Number').find('input').type('0682691386')
+        cy.contains('Email').find('input').type('sipho@gmail.com').should('have.value', 'sipho@gmail.com')
+        cy.contains('Submit Application').click()
+
+
     })
+
+    it.skip('enters invalid numbers as text input', ()=>{
+        cy.contains('Company Name *').find('input').type('5258')
+        cy.contains('Contact Person *').find('input').type('885')
+        cy.contains('About Company *').find('textarea').type('856')
+        
+    })
+
 })
+
 
 
