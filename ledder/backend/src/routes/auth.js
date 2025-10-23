5a5cc05658e1b514ac4bd7cb06f63069754dd68f
siphoprince <mabirimisesipho@gmail.com>
2025-10-23 13:56:08 +0200

    adding auth for register

--- ledder/backend/src/routes/auth.js
+++ ledder/backend/src/routes/auth.js
@@ -1,6 +1,24 @@
-var express = require('express');
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const db = require('./db');
 
-var router = express.Router();
+//register
+router.post('/register', async (req, res) =>{
+  const {email, password} = req.body;
+  if(!email || !password) return res.status(400).json({message: 'email and password required'});
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  db.run(`INSERT INTO users(email, password) VALUES(?, ?)`, [email, hashedPassword], function(err){
+    if(err) return res.status(400).json({message: 'User already exists or error'});
+    res.json({message:'user registered successfully!'})
+  });
+});
+
+
+
 
 router.get('/login', function(req, res, next) {
   res.render('login');
